3334f7fc2aa25fe96ead2ab6d39469aad7f2e5ba
ROUVELL <sheva15032005@gmail.com>
2023-01-18 19:57:58 +0100

    IT WORKS !!!

--- world.py
+++ world.py
@@ -5,7 +5,7 @@ from config import TILE_SIZE
 
 _ = None
 _map = [
-    [_, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, _],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
@@ -13,11 +13,11 @@ _map = [
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [_, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, _]
+    [1, _, _, _, _, 1, 1, _, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 
@@ -33,36 +33,20 @@ class World:
                     self.tiles.append(pg.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     def check_collide(self, target: pg.Rect, direction: vec) -> vec:
-        indexes = target.move(direction).collidelistall(self.tiles)
-        colliders = [self.tiles[i] for i in indexes]
-        offset = vec()
-        vo, ho = direction
 
+        def collide(target: pg.Rect, offset: vec) -> vec:
+            if not offset: return vec()
+            # horizontal
+            if target.move(offset.x, 0).collidelistall(self.tiles):
+                new_offset = vec(offset.x - 1 if offset.x > 0 else offset.x + 1, offset.y)
+                return collide(target, new_offset)
+            # vertical
+            if target.move(0, offset.y).collidelistall(self.tiles):
+                new_offset = vec(offset.x, offset.y - 1 if offset.y > 0 else offset.y + 1)
+                return collide(target, new_offset)
+            return offset
 
-        if not colliders: return direction
-        if len(colliders) == 1: return vec(direction.x, 0)
-        if len(colliders) == 2:
-            rect1, rect2 = colliders
-
-        if rect1.centerx == rect2.centerx:
-            # пересікли зліва
-            if abs(target.centerx - rect1.right) < abs(target.centerx - rect1.left):
-                offset.x = direction.x - (rect1.right - target.left)
-            # пересікли зправа
-            else: offset.x = direction.x - (target.right - rect1.left)
-        # два горизонтально
-        elif rect1.centery == rect2.centery:
-            # пересікли зверху
-            if abs(target.centery - rect1.bottom) < abs(target.centery - rect1.top):
-                offset.y = direction.y - (rect1.bottom - target.top)
-            # пересікли знизу
-            else: offset.y = direction.y - (target.bottom - rect1.top)
-        # один вище другий нижче
-        else:
-            pass
-
-        return offset
-
+        return collide(target, direction)
 
     def update(self):
         pass
