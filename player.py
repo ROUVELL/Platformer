a3900c828a5d87e6f728ff7ad5f4cc6bfac8ed32
ROUVELL <sheva15032005@gmail.com>
2023-01-19 11:02:50 +0100

    name bliting

--- player.py
+++ player.py
@@ -5,9 +5,10 @@ from config import *
 
 
 class Player:
-    def __init__(self, game):
+    def __init__(self, game, name: str = 'Player'):
         self.game = game
-        self._rect = pg.Rect(PLAYER_POS, PLAYER_SIZE)
+        self.name = pg.font.SysFont('arial', 14).render(name, True, 'white')
+        self.rect = pg.Rect(PLAYER_POS, PLAYER_SIZE)
         #############
         self.speed = PLAYER_SPEED  # швидкість ігрока
         self.direction = vec()     # напрям руху
@@ -25,12 +26,16 @@ class Player:
 
         self.direction.y = min(self.direction.y + GRAVITY, MAX_VERTICAL_SPEED)
 
-        self.direction = self.game.world.check_collide(self._rect.copy(), self.direction)
+        self.direction = self.game.world.check_collide(self.rect.copy(), self.direction)
 
-        self._rect.move_ip(self.direction)
+        self.rect.move_ip(self.direction)
 
     def update(self):
         self.movement()
 
     def draw(self, sc: pg.Surface):
-        pg.draw.rect(sc, 'red', self._rect, border_radius=3)
+        pg.draw.rect(sc, 'red', self.rect, border_radius=3)
+        rect = self.name.get_rect()
+        rect.centerx = self.rect.centerx
+        rect.bottom = self.rect.top - 2
+        sc.blit(self.name, rect)
