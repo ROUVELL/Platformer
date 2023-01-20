e0cea67e8ca03fe11c3645915d949a7ad396bcc7
ROUVELL <sheva15032005@gmail.com>
2023-01-20 14:00:16 +0100

    timer file

---
+++ timer.py
@@ -0,0 +1,19 @@
+from pygame.time import get_ticks
+
+
+class Timer:
+    def __init__(self, duration: int):
+        self.last_update = get_ticks()
+        self.duration = duration
+        ##########
+        self.state = False   # True якщо час вийшов інакше False
+
+    def update(self):
+        if not self.state:
+            now = get_ticks()
+            if now - self.last_update >= self.duration:
+                self.state = True
+
+    def activate(self):
+        self.last_update = get_ticks()
+        self.state = False
