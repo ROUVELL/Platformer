fff62df0839cea65e031a34254244997639aaff4
ROUVELL <sheva15032005@gmail.com>
2023-01-20 20:06:26 +0100

    refactoring

--- bullet.py
+++ bullet.py
@@ -5,43 +5,38 @@ from config import AUTO_REMOVE_DELEY
 
 
 # Abstract class
-class Bullet:
-    def __init__(self, group: list, pos: vec, size: vec, direction: vec, *, type_: str = 'rect', color: str = 'blue'):
-        assert type_ in ['rect', 'cicle'], f'Unknown type of bullet: {type_}!'
+class __Bullet:
+    def __init__(self, group: list, pos: vec, size: vec, direction: vec, *, color: str = 'orange'):
         assert direction, 'Direction can`t be (0, 0)!'
-        self.group = group
-        self.pos = pos
-        self.size = size
-        self.direction = direction
-        self.type_ = type_
-        self.color = color
+        self._group = group
+        self._pos = pos
+        self._size = size
+        self._direction = direction
+        self._color = color
         # Таймер самознищення
-        self.remove_timer = Timer(AUTO_REMOVE_DELEY, activate=True)
+        self._remove_timer = Timer(AUTO_REMOVE_DELEY, activate=True)
 
     @property
-    def rect(self):
-        return pg.Rect(self.pos, self.size)
+    def rect(self) -> pg.Rect:
+        return pg.Rect(self._pos, self._size)
 
     def move(self, offset: vec):
-        self.pos += offset
+        self._pos += offset
 
     def update(self):
-        self.remove_timer.update()
-        if self.remove_timer: self.group.remove(self)
-        self.move(self.direction)
+        self._remove_timer.update()
+        if self._remove_timer: self._group.remove(self)
+        self.move(self._direction)
 
     def draw(self, sc: pg.Surface):
-        if self.type_ == 'rect':
-            pg.draw.rect(sc, self.color, self.rect, border_radius=3)
-        elif self.type_ == 'cicle':
-            pg.draw.circle(sc, self.color, self.rect.center, self.size.x)
+        pg.draw.circle(sc, self._color, self.rect.center, self._size.x)
 
 
-class GlockBullet(Bullet):
+class GlockBullet(__Bullet):
     def __init__(self, group: list, pos: vec, direction: vec):
-        super().__init__(group, pos, vec(3, 3), direction, type_='cicle', color='orange')
+        super().__init__(group, pos, vec(3, 3), direction)
 
 
-class Ak47Bullet(Bullet):
+class Ak47Bullet(__Bullet):
     def __init__(self, group: list, pos: vec, direction: vec):
-        super().__init__(group, pos, vec(4, 4), direction, type_='cicle', color='orange')
+        super().__init__(group, pos, vec(4, 4), direction)
