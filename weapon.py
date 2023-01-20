e60ccbcc99b21b73fbdb35bfb7da8f2cbb9ba268
ROUVELL <sheva15032005@gmail.com>
2023-01-20 19:26:39 +0100

    reloading and bug fix

--- weapon.py
+++ weapon.py
@@ -1,31 +1,55 @@
 import pygame as pg
 
-from bullet import Bullet, PistolBullet
+from bullet import GlockBullet, Ak47Bullet
 from utills import vec, all_events, Timer, current_pos
+from config import CENTER
 
 
 # Abstract class
 class Weapon:
-    def __init__(self, user, group: list, size: tuple[int, int], shot_deley: int = 100, name: str = 'Gun'):
+    def __init__(self, user, group: list, *, size: tuple[int, int],
+                 shot_deley: int = 100, reload_time: int = 2000, name: str = 'Gun'):
         rect = pg.Rect((0, 0), size)
         rect.center = user.rect.center
         self.rect = rect
         self.user = user
         self.bullet_group = group
+        self.bullet = GlockBullet
+        self.magazine_size = 30
+        self.curr_bullets = 0
         ##########
-        self.shot_pos = vec(self.rect.right, self.rect.top)  # Позиція де з'являється пуля
         self.shot_timer = Timer(shot_deley)
+        self.reload_timer = Timer(reload_time)
+        self.reloading = False
         ##########
         self.name = pg.font.SysFont('calibri', 12).render(name, True, 'white')
+        self.reloading_text = pg.font.SysFont('calibri', 34).render('Reloading', True, 'white')
 
     def shot(self):
-        if self.shot_timer.state:
+        if self.shot_timer and self.reload_timer:
+            self.curr_bullets -= 1
             self.shot_timer.activate()
-            self.bullet_group.append(Bullet(self.bullet_group, self.shot_pos, vec(12, 5), vec(5, 0), type_='rect', color='blue'))
+            size = vec(current_pos()) - vec(self.rect.center)
+            direction = vec(size.x / size.length(), size.y / size.length())
+            direction *= 4
+            self.bullet_group.append(self.bullet(self.bullet_group, vec(self.rect.center), direction))
+
+    def reload(self):
+        # Якщо таймер не активований і ми не перезаряжаємось - починаємо перезарядку
+        # Якщо таймер не активований і ми все ще перезаряєжаємось - значить перезарядку закінчено
+        if not self.reload_timer: return
+        if self.reloading:
+            self.curr_bullets = self.magazine_size
+            self.reloading = False
+        else:
+            self.reload_timer.activate()
+            self.reloading = True
 
     def update(self):
         self.shot_timer.update()
-        self.shot_pos = vec(self.rect.right, self.rect.top)
+        self.reload_timer.update()
+        if self.reloading or not self.curr_bullets:
+            self.reload()
 
     def draw(self, sc: pg.Surface):
         name_rect = self.name.get_rect()
@@ -33,17 +57,19 @@ class Weapon:
         name_rect.bottom = self.rect.top - 2
 
         pg.draw.rect(sc, 'green', self.rect, border_radius=3)
+        if not self.reload_timer:
+            rect = self.reloading_text.get_rect(center=CENTER)
+            sc.blit(self.reloading_text, rect)
         # sc.blit(self.name, name_rect)
 
 
-class Pistol(Weapon):
+class Glock(Weapon):
     def __init__(self, user, group: list):
-        super().__init__(user, group, (20, 15), 3, 'Pistol')
+        super().__init__(user, group, size=(20, 15), shot_deley=300, reload_time=3000, name='Glock')
+        self.magazine_size = 7
 
-    def shot(self):
-        if self.shot_timer.state:
-            self.shot_timer.activate()
-            size = vec(current_pos()) - self.shot_pos
-            direction = vec(size.x / size.length(), size.y / size.length())
-            direction *= 4
-            self.bullet_group.append(PistolBullet(self.bullet_group, self.shot_pos, direction))
+
+class Ak47(Weapon):
+    def __init__(self, user, group: list):
+        super().__init__(user, group, size=(30, 15), shot_deley=80, reload_time=2700, name='Ak-47')
+        self.bullet = Ak47Bullet
