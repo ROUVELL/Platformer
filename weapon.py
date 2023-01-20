ff1b7c6b6716d42447238a0daf9e95ed9e3ced92
ROUVELL <sheva15032005@gmail.com>
2023-01-20 20:06:45 +0100

    refactoring and some comments

--- weapon.py
+++ weapon.py
@@ -12,54 +12,56 @@ class Weapon:
         rect = pg.Rect((0, 0), size)
         rect.center = user.rect.center
         self.rect = rect
-        self.user = user
-        self.bullet_group = group
-        self.bullet = GlockBullet
-        self.magazine_size = 30
-        self.curr_bullets = 0
+        self.user = user  # Той хто використовує зброю
+        self._bullet_group = group  # група для патронів
+        self.bullet = GlockBullet  # клас набоя яким будемо стріляти
+        self.magazine_size = 30  # місткість магазину
+        self.curr_bullets = 0  # поточна к-сть набоїв в магазині
         ##########
-        self.shot_timer = Timer(shot_deley)
-        self.reload_timer = Timer(reload_time)
-        self.reloading = False
+        self._shot_timer = Timer(shot_deley)  # таймер між вистрілами
+        self._reload_timer = Timer(reload_time)  # таймер перезарядки
+        self.reloading = False  # чи перезаряжаємо
         ##########
-        self.name = pg.font.SysFont('calibri', 12).render(name, True, 'white')
-        self.reloading_text = pg.font.SysFont('calibri', 34).render('Reloading', True, 'white')
+        self._name = pg.font.SysFont('calibri', 12).render(name, True, 'white')
+        self._reloading_text = pg.font.SysFont('calibri', 36).render('Reloading', True, 'white')
+
+    def _get_direction(self) -> vec:
+        size = current_pos() - vec(self.rect.center)
+        direction = vec(size.x / size.length(), size.y / size.length())
+        return direction * 4
 
     def shot(self):
-        if self.shot_timer and self.reload_timer:
+        if self._shot_timer and self._reload_timer:
             self.curr_bullets -= 1
-            self.shot_timer.activate()
-            size = vec(current_pos()) - vec(self.rect.center)
-            direction = vec(size.x / size.length(), size.y / size.length())
-            direction *= 4
-            self.bullet_group.append(self.bullet(self.bullet_group, vec(self.rect.center), direction))
+            self._shot_timer.activate()
+            self._bullet_group.append(self.bullet(self._bullet_group, vec(self.rect.center), self._get_direction()))
 
     def reload(self):
         # Якщо таймер не активований і ми не перезаряжаємось - починаємо перезарядку
         # Якщо таймер не активований і ми все ще перезаряєжаємось - значить перезарядку закінчено
-        if not self.reload_timer: return
+        if not self._reload_timer: return
         if self.reloading:
             self.curr_bullets = self.magazine_size
             self.reloading = False
         else:
-            self.reload_timer.activate()
+            self._reload_timer.activate()
             self.reloading = True
 
     def update(self):
-        self.shot_timer.update()
-        self.reload_timer.update()
+        self._shot_timer.update()
+        self._reload_timer.update()
         if self.reloading or not self.curr_bullets:
             self.reload()
 
     def draw(self, sc: pg.Surface):
-        name_rect = self.name.get_rect()
+        name_rect = self._name.get_rect()
         name_rect.centerx = self.rect.centerx
         name_rect.bottom = self.rect.top - 2
 
         pg.draw.rect(sc, 'green', self.rect, border_radius=3)
-        if not self.reload_timer:
-            rect = self.reloading_text.get_rect(center=CENTER)
-            sc.blit(self.reloading_text, rect)
+        if not self._reload_timer:
+            rect = self._reloading_text.get_rect(center=CENTER)
+            sc.blit(self._reloading_text, rect)
         # sc.blit(self.name, name_rect)
 
 
