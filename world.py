588c8651978b61caab72772f60f4364880054e55
ROUVELL <sheva15032005@gmail.com>
2023-01-22 10:39:43 +0100

    change collide func

--- world.py
+++ world.py
@@ -5,19 +5,19 @@ from config import TILE_SIZE
 
 _ = None
 _map = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [_, _, 1, _, _, _, _, _, _, _, _, _, _, _, _, 1, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, 1, 1, _, _, _, _, _, 1, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, 1, 1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, 1, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [1, 1, _, _, _, _, _, _, _, 1, 1, _, _, _, _, _, 1, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, 1, _, _, _, _, _, 1, 1, _, _, _, _, _, _, _, 1],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
@@ -35,20 +35,24 @@ class World:
                     self.tiles.append(pg.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     def check_collide(self, target: pg.Rect, direction: vec) -> vec:
+        def horizontal_collide(target: pg.Rect, ox: float) -> float:
+            if not int(ox): return ox
+            if target.move(ox, 0).collidelist(self.tiles) != -1:
+                new_ox = ox - 1 if ox > 0 else ox + 1
+                return horizontal_collide(target, new_ox)
+            return ox
 
-        def collide(target: pg.Rect, offset: vec) -> vec:
-            if not offset: return vec()
-            # horizontal
-            if target.move(offset.x, 0).collidelist(self.tiles) != -1:
-                new_offset = vec(offset.x - 1 if offset.x > 0 else offset.x + 1, offset.y)
-                return collide(target, new_offset)
-            # vertical
-            if target.move(0, offset.y).collidelist(self.tiles) != -1:
-                new_offset = vec(offset.x, offset.y - 1 if offset.y > 0 else offset.y + 1)
-                return collide(target, new_offset)
-            return offset
+        def vertical_collide(target: pg.Rect, oy: float) -> float:
+            if not int(oy): return oy
+            if target.move(0, oy).collidelist(self.tiles) != -1:
+                new_oy = oy - 1 if oy > 0 else oy + 1
+                return vertical_collide(target, new_oy)
+            return oy
 
-        return collide(target, direction)
+        ox = horizontal_collide(target, direction.x)
+        oy = vertical_collide(target, direction.y)
+
+        return vec(ox, oy)
 
     def offset(self, offset: vec):
         [rect.move_ip(offset) for rect in self.tiles]
